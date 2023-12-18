6ba0e5a9f608b539af7f03532d29d9e5dff139b8
diegolentz <you@example.com>
2023-12-18 17:08:42 -0300

    first commit

---
+++ src/main/java/ar/com/escondi2/Springg/SpringgApplication.java
@@ -0,0 +1,14 @@
+package ar.com.escondi2.Springg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(SpringgApplication.class, args);
+		System.out.println("hola como te va");
+	}
+
+}
---
+++ src/test/java/ar/com/escondi2/Springg/SpringgApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.escondi2.Springg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
