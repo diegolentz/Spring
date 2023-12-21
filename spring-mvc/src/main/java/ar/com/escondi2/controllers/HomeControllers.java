701d179e99b91ae1e5ecd464ec49e2a325462e6d
diegolentz <you@example.com>
2023-12-21 20:22:38 -0300

    hasta aca llegue

--- spring-mvc/src/main/java/ar/com/escondi2/controllers/HomeControllers.java
+++ spring-mvc/src/main/java/ar/com/escondi2/controllers/HomeControllers.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@SuppressWarnings("unused")
 @Controller
 public class HomeControllers {
 	
